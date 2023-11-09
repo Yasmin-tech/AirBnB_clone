1b0e855ad4366516bca767542d9601df2f278c62
Olakunle64 <olakunleisiaq50@gmail.com>
2023-11-09 00:05:21 +0000

    edit the console.py

--- console.py
+++ console.py
@@ -3,7 +3,8 @@
     i.e, the entry point for the command intepreter
     """
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """This class is a command intepreter"""
@@ -20,6 +21,35 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self, line):
         """do nothing when the line is empty"""
         pass
+    def do_create(self, line):
+        """create an instance of BaseModel"""
+        if not line:
+            print("** class name missing **")
+        else:
+            if line == "BaseModel":
+                b1 = BaseModel()
+                b1.save()
+                print(b1.id)
+            else:
+                print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """print the string implementation of an instance based
+        on the class name and the id
+        """
+        if not line:
+            print("** class name missing **")
+        else:
+            class_name, id_value = line.splits()
+            if class_name != "BaseModel":
+                print("** class doesn't exist **")
+            elif not id_value:
+                print("** instance id missing **")
+            else:
+                with open("file.json", "r", encoding="utf-8") as f:
+                    all_obj = json.load(f)
+            if all_obj[
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
